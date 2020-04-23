8765ed9a94b500ca0f4830d4de726851c47ab53b
Jocelin <hounonj@gmail.com>
2020-04-23 14:38:41 +0100

    adresse dialog

--- lib/ui/components/HomeAppBar.dart
+++ lib/ui/components/HomeAppBar.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nekxolivro/ui/components/JDivider.dart';
@@ -5,7 +7,6 @@ import 'package:nekxolivro/utils/dialog_inseat_defeat.dart';
 import 'package:nekxolivro/values/JIcon.dart';
 import 'package:nekxolivro/values/Palette.dart';
 import 'package:nekxolivro/values/StringRes.dart';
-import 'package:nekxolivro/values/Styles.dart';
 
 class HomeAppBar extends StatefulWidget {
   @override
@@ -13,7 +14,8 @@ class HomeAppBar extends StatefulWidget {
 }
 
 class HomeAppBarState extends State<HomeAppBar> {
-  int aId = 0;
+  int aId = -1;
+  List<String> adresses = ["Au Bureau", "Chez Blanche", "Au Carrefour"];
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +43,9 @@ class HomeAppBarState extends State<HomeAppBar> {
                         padding: const EdgeInsets.symmetric(vertical: 2.0),
                         child: RichText(
                             text: TextSpan(
-                                text: "Emplacement actuel",
+                                text: aId == -1
+                                    ? "Emplacement actuel"
+                                    : adresses[aId],
                                 style: TextStyle(
                                     color: Palette.colorBlueBlack,
                                     fontFamily: StringRes.Avenir_Heavy,
@@ -149,6 +153,149 @@ class HomeAppBarState extends State<HomeAppBar> {
         edgeInsets: EdgeInsets.symmetric(horizontal: 8),
         builder: (context) {
           double dialogRadius = 5.0;
+          List<Widget> children = [];
+          print("aId $aId");
+          children.addAll([
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+              child: Text(
+                "Sélectionner l'adresse de livraison",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: StringRes.Avenir_Heavy,
+                    color: Palette.colorBlueBlack),
+              ),
+            ),
+            JDivider(),
+            InkWell(
+              onTap: () => setAdresse(-1),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.place,
+                      color: Palette.greyText,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                        child: Text(
+                      "Emplacement actuel",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: StringRes.Avenir_Book,
+                      ),
+                    )),
+                    if (aId == -1)
+                      Icon(
+                        Icons.check_circle,
+                        color: Palette.colorSecondary,
+                      )
+                  ],
+                ),
+              ),
+            ),
+            JDivider(),
+          ]);
+          for (int i = 0; i < min(adresses.length, 2); i++) {
+            children.addAll([
+              InkWell(
+                onTap: () => setAdresse(i),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.flag,
+                        color: Palette.greyText,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                          child: Text(
+                        "${adresses[i]}",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: StringRes.Avenir_Book,
+                        ),
+                      )),
+                      if (aId == i)
+                        Icon(
+                          Icons.check_circle,
+                          color: Palette.colorSecondary,
+                        )
+                    ],
+                  ),
+                ),
+              ),
+              JDivider(),
+            ]);
+          }
+
+          if (adresses.length > 2)
+            children.addAll([
+              InkWell(
+                onTap: () => Navigator.pop(context),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                              text: "Voir tout",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: Palette.colorBlueBlack,
+                                  fontFamily: StringRes.Avenir_Heavy),
+                              children: [
+                                WidgetSpan(
+                                    child: Icon(
+                                  CupertinoIcons.right_chevron,
+                                  size: 15,
+                                ))
+                              ]),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              JDivider()
+            ]);
+          children.add(InkWell(
+            onTap: () => null,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.add,
+                    color: Palette.colorPrimary,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                      child: Text(
+                    "Nouvelle adresse de livraison",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Palette.colorPrimary,
+                      fontFamily: StringRes.Avenir_Book,
+                    ),
+                  )),
+                ],
+              ),
+            ),
+          ));
           return Dialog(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(dialogRadius)),
@@ -157,213 +304,15 @@ class HomeAppBarState extends State<HomeAppBar> {
                   width: double.infinity,
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 15, horizontal: 10),
-                        child: Text(
-                          "Sélectionner l'adresse de livraison",
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontFamily: StringRes.Avenir_Heavy,
-                              color: Palette.colorBlueBlack),
-                        ),
-                      ),
-                      JDivider(),
-                      Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.place,
-                              color: Palette.greyText,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Expanded(
-                                child: Text(
-                              "Emplacement actuel",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontFamily: StringRes.Avenir_Book,
-                              ),
-                            )),
-                          ],
-                        ),
-                      ),
-                      JDivider(),
-                      Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.flag,
-                              color: Palette.greyText,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Expanded(
-                                child: Text(
-                              "Au Bureau",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontFamily: StringRes.Avenir_Book,
-                              ),
-                            )),
-                          ],
-                        ),
-                      ),
-                      JDivider(),
-                      Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.flag,
-                              color: Palette.greyText,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Expanded(
-                                child: Text(
-                              "Chez Blanche",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontFamily: StringRes.Avenir_Book,
-                              ),
-                            )),
-                            Icon(Icons.check_circle,color: Palette.colorBlue,)
-                          ],
-                        ),
-                      ),
-                      JDivider(),
-                      InkWell(
-                        onTap: () => null,
-                        child: Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Row(
-                            children: <Widget>[
-                              Icon(
-                                Icons.add,
-                                color: Palette.colorPrimary,
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Expanded(
-                                  child: Text(
-                                "Nouvelle adresse de livraison",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Palette.colorPrimary,
-                                  fontFamily: StringRes.Avenir_Book,
-                                ),
-                              )),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
+                    children: children,
                   )));
         });
   }
 
-  _adressBottomDialog(context) {
-    showModalBottomSheet(
-        context: context,
-        backgroundColor: Palette.whiteBackGround,
-        builder: (context) {
-          List<Widget> children = [];
-          children.add(SliverList(
-            delegate: SliverChildListDelegate([
-              Text(
-                "Adresse de Livraison",
-                style: Styles.mediumBlueBlackTitle,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  onTap: () => null,
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    width: double.infinity,
-                    child: RichText(
-                      text: TextSpan(
-                          style: TextStyle(
-                              color: Palette.colorPrimary,
-                              fontSize: 16,
-                              fontFamily: StringRes.Avenir_Light,
-                              fontWeight: FontWeight.bold),
-                          children: [
-                            WidgetSpan(
-                                child: Icon(
-                              Icons.add,
-                              size: 20,
-                              color: Palette.colorPrimary,
-                            )),
-                            WidgetSpan(
-                                child: SizedBox(
-                              width: 10,
-                            )),
-                            TextSpan(text: "Nouvelle adresse de livraison")
-                          ]),
-                    ),
-                  ),
-                ),
-              ),
-            ]),
-          ));
-          children.add(SliverList(
-              delegate: SliverChildBuilderDelegate(
-            (builder, index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                child: adresseRadio(index),
-              );
-            },
-            childCount: 10,
-          )));
-          return Container(
-            padding: EdgeInsets.only(left: 15, top: 15, right: 15, bottom: 30),
-            child: CustomScrollView(
-              slivers: children,
-            ),
-          );
-        });
+  setAdresse(int idx) {
+    setState(() {
+      aId = idx;
+    });
+    Navigator.pop(context);
   }
-
-  Widget adresseRadio(index) => Row(
-        children: <Widget>[
-          Icon(
-            CupertinoIcons.location_solid,
-            color: Palette.greyText,
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          Text(
-            "Lieu actuel",
-            style: TextStyle(
-              fontSize: 16,
-              fontFamily: StringRes.Avenir_Light,
-            ),
-          ),
-          Spacer(),
-          Radio(
-              value: index,
-              groupValue: aId,
-              onChanged: (val) {
-                setState(() {
-                  print('change');
-                  aId = val;
-                });
-              })
-        ],
-      );
 }
